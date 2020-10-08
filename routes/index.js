69523aff8d651d25568fb70ca6ade10b236e4eea
Hanna <hweissbe@g.jct.ac.il>
2020-10-08 17:01:19 +0300

    routes update1

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 router.use("/", mainRoute);
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, '../in-posters/build/index.html'));
+    res.sendFile(path.join(__dirname, 'in-posters/build/index.html'));
 });
 
 module.exports = router;
